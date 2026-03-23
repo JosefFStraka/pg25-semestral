615720436d72e071d0fd7ac19e2edbef1dadde5b
Josef František Straka <strajo186@gmail.com>
2026-03-23 17:12:35 +0100

    camera fix + cc

--- camera.hpp
+++ camera.hpp
@@ -11,38 +11,35 @@ public:
     // Camera Attributes
     glm::vec3 Position{};
     glm::vec3 Front{};
-    glm::vec3 Right{}; 
+    glm::vec3 Right{};
     glm::vec3 Up{}; // camera local UP vector
 
     GLfloat Yaw = -90.0f;
-    GLfloat Pitch =  0.0f;;
+    GLfloat Pitch = 0.0f;;
     GLfloat Roll = 0.0f;
-    
+
     // Camera options
     GLfloat MovementSpeed = 1.0f;
     GLfloat MouseSensitivity = 0.25f;
-    
-   Camera() {
+
+    Camera() {
         // Default constructor initializes camera's position and orientation
         this->updateCameraVectors();
     }
 
-    Camera(glm::vec3 position):Position(position)
-    {
-        this->Up = glm::vec3(0.0f,1.0f,0.0f);
+    Camera(glm::vec3 position) :Position(position) {
+        this->Up = glm::vec3(0.0f, 1.0f, 0.0f);
         // initialization of the camera reference system
         this->updateCameraVectors();
     }
 
-    glm::mat4 GetViewMatrix()
-    {
+    glm::mat4 GetViewMatrix() {
         return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
     }
 
-    glm::vec3 ProcessInput(GLFWwindow* window, GLfloat deltaTime)
-    {
-        glm::vec3 direction{0};
-          
+    glm::vec3 ProcessInput(GLFWwindow* window, GLfloat deltaTime) {
+        glm::vec3 direction{ 0 };
+
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
             direction += Front; // add unit vector to final direction  
 
@@ -50,26 +47,27 @@ public:
             direction -= Front;
 
         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-            direction -= Right;       
+            direction -= Right;
 
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
             direction += Right;
 
         //... up, down, diagonal, ... 
 
+        if (glm::length(direction) == 0)
+            return glm::vec3(0.f, 0.f, 0.f);
+
         return glm::normalize(direction) * MovementSpeed * deltaTime;
     }
 
-    void ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constraintPitch = GL_TRUE)
-    {
+    void ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constraintPitch = GL_TRUE) {
         xoffset *= this->MouseSensitivity;
         yoffset *= this->MouseSensitivity;
 
-        this->Yaw   += xoffset;
+        this->Yaw += xoffset;
         this->Pitch += yoffset;
 
-        if (constraintPitch)
-        {
+        if (constraintPitch) {
             if (this->Pitch > 89.0f)
                 this->Pitch = 89.0f;
             if (this->Pitch < -89.0f)
@@ -88,6 +86,6 @@ private:
 
         this->Front = glm::normalize(front);
         this->Right = glm::normalize(glm::cross(this->Front, glm::vec3(0.0f, 1.0f, 0.0f)));
-        this->Up    = glm::normalize(glm::cross(this->Right, this->Front));
+        this->Up = glm::normalize(glm::cross(this->Right, this->Front));
     }
 };
