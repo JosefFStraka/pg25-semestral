184c42be78c44a2321e1e7a47153fce3f4adae29
Josef František Straka <strajo186@gmail.com>
2026-03-23 16:44:27 +0100

    improved obj loader

--- OBJloader.cpp
+++ OBJloader.cpp
@@ -3,89 +3,154 @@
 #include <GL/glew.h> 
 #include <glm/glm.hpp>
 #include <iostream>
+#include <unordered_map>
 
 #include "OBJloader.hpp"
 
-#define MAX_LINE_SIZE 1024
+#define MAX_LINE_SIZE 256 //should not be longer
+#define MAX_VERTICES 3
+#define NO_INDEX    -1
 
-bool loadOBJ(const std::filesystem::path& filename, std::vector<Vertex>& vertices, std::vector<GLuint>& indices)
-{
+long resolve_position(long number, size_t max) {
+	if (number < 0) {
+		return max + number;
+	}
+	return number - 1;
+}
+
+typedef struct {
+	int vertex[MAX_VERTICES];
+	int tex[MAX_VERTICES];
+	int normal[MAX_VERTICES];
+} Face;
+
+int parseFaceLine(const char* line, Face* face) {
+	// Initialize all indices to NO_INDEX
+	for (int i = 0; i < MAX_VERTICES; i++)
+		face->vertex[i] = face->tex[i] = face->normal[i] = NO_INDEX;
+
+	// Try v/vt/vn first
+	int n = sscanf_s(line,
+		"%d/%d/%d %d/%d/%d %d/%d/%d",
+		&face->vertex[0], &face->tex[0], &face->normal[0],
+		&face->vertex[1], &face->tex[1], &face->normal[1],
+		&face->vertex[2], &face->tex[2], &face->normal[2]);
+	if (n == 9) return 4;
+
+	// Try v//vn (no texture)
+	n = sscanf_s(line,
+		"%d//%d %d//%d %d//%d",
+		&face->vertex[0], &face->normal[0],
+		&face->vertex[1], &face->normal[1],
+		&face->vertex[2], &face->normal[2]);
+	if (n == 6) return 3;
+
+	// Try v/vt (no normals)
+	n = sscanf_s(line,
+		"%d/%d %d/%d %d/%d",
+		&face->vertex[0], &face->tex[0],
+		&face->vertex[1], &face->tex[1],
+		&face->vertex[2], &face->tex[2]);
+	if (n == 6) return 2;
+
+	// Try vertex only
+	n = sscanf_s(line, "%d %d %d",
+		&face->vertex[0],
+		&face->vertex[1],
+		&face->vertex[2]);
+	if (n == 3) return 1;
+
+	return 0; // failed to parse
+}
+
+
+bool loadOBJ(const std::filesystem::path& filename, std::vector<Vertex>& vertices, std::vector<GLuint>& indices) {
 	std::cout << "Loading model: " << filename.string() << std::endl;
 
 	std::vector< glm::vec3 > temp_vertices;
 	std::vector< glm::vec2 > temp_uvs;
 	std::vector< glm::vec3 > temp_normals;
 
+	std::unordered_map<Vertex, GLuint, VertexHasher> vertexCache;
+
 	vertices.clear();
 	indices.clear();
 
-	FILE * file = nullptr;
+	FILE* file = nullptr;
 	fopen_s(&file, filename.string().c_str(), "r");
 	if (file == NULL) {
 		printf("Impossible to open the file !\n");
 		return false;
 	}
 
+	int line_number = 0;
 	while (1) {
-
-		char lineHeader[MAX_LINE_SIZE];
-		int res = fscanf_s(file, "%s", lineHeader, MAX_LINE_SIZE);
-		if (res == EOF) {
+		line_number++;
+		char line[MAX_LINE_SIZE];
+		if (!fgets(line, sizeof(line), file)) {
 			break;
 		}
 
+		char lineHeader[3];
+		int res = sscanf_s(line, "%2s", lineHeader, 3);
+		if (res == EOF) {
+			continue;
+		}
 		if (strcmp(lineHeader, "v") == 0) {
 			glm::vec3 vertex;
-			fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			sscanf_s(line + 2, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
 			temp_vertices.push_back(vertex);
-		}
-		else if (strcmp(lineHeader, "vt") == 0) {
+		} else if (strcmp(lineHeader, "vt") == 0) {
 			glm::vec2 uv;
-			fscanf_s(file, "%f %f\n", &uv.x, &uv.y);
+			sscanf_s(line + 2, "%f %f\n", &uv.x, &uv.y);
 			temp_uvs.push_back(uv);
-		}
-		else if (strcmp(lineHeader, "vn") == 0) {
+		} else if (strcmp(lineHeader, "vn") == 0) {
 			glm::vec3 normal;
-			fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			sscanf_s(line + 2, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
 			temp_normals.push_back(normal);
-		}
-		else if (strcmp(lineHeader, "f") == 0) {
-			std::string vertex1, vertex2, vertex3;
-			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf_s(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-			if (matches != 9) {
-				printf("File can't be read by simple parser :( Try exporting with other options\n");
-				return false;
+		} else if (strcmp(lineHeader, "f") == 0) {
+			Face face;
+			int result = parseFaceLine(line + 2, &face);
+			if (result == 0) {
+				printf("File can't be read by simple parser :( Try exporting with other options\n\t\"%s\"\n", line + 2);
 			}
 
 			for (int i = 0; i < 3; i++) {
 				GLuint currentIndex;
 				Vertex currentVertex;
-				currentVertex.position = temp_vertices[vertexIndex[i]-1]; // OBJ array start from 1
-				currentVertex.normal = temp_normals[normalIndex[i]-1];
-				currentVertex.texCoords = temp_uvs[uvIndex[i]-1];
+				long vi = resolve_position(face.vertex[i], temp_vertices.size());
+				// if (vi < 0 || vi >= temp_vertices.size()) {
+				// 	printf("erm %d\n", line_number);
+				// 	continue; // skip invalid
+				// }
 
-                // avoid duplicit vertices
-				auto t = std::find_if(vertices.begin(),
-					vertices.end(),
-					[&currentVertex]
-					(const Vertex& v2) -> bool {
-						return (currentVertex == v2);
-					});
-                    
-				if (t == vertices.end()) {
+				currentVertex.position = temp_vertices[vi]; // OBJ array start from 1
+
+				if (result == 4 || result == 2)
+					currentVertex.texCoords = temp_uvs[resolve_position(face.tex[i], temp_uvs.size())];
+				else
+					currentVertex.texCoords = glm::vec2();
+
+				if (result == 4 || result == 3)
+					currentVertex.normal = temp_normals[resolve_position(face.normal[i], temp_normals.size())];
+				else
+					currentVertex.normal = glm::vec3();
+
+				// avoid duplicit vertices
+				auto it = vertexCache.find(currentVertex);
+				GLuint index;
+				if (it == vertexCache.end()) {
+					index = vertices.size();
 					vertices.push_back(currentVertex);
-					currentIndex = vertices.size() - 1;
+					vertexCache[currentVertex] = index;
+				} else {
+					index = it->second;
 				}
-				else {
-					currentIndex = t - vertices.begin();
-				}
-				indices.push_back(currentIndex);
+				indices.push_back(index);
 			}
-
 		}
 	}
-	
+
 	std::cout << "Model loaded: " << filename.string() << std::endl;
 
 	fclose(file);
--- assets.hpp
+++ assets.hpp
@@ -21,4 +21,26 @@ struct Vertex {
     }
 };
 
+// Hash function for glm::vec types
+struct VertexHasher {
+    size_t operator()(const Vertex& v) const {
+        auto hashVec3 = [](const glm::vec3& vec) -> size_t {
+            size_t h1 = std::hash<float>()(vec.x);
+            size_t h2 = std::hash<float>()(vec.y);
+            size_t h3 = std::hash<float>()(vec.z);
+            return ((h1 ^ (h2 << 1)) >> 1) ^ h3;
+        };
+        auto hashVec2 = [](const glm::vec2& vec) -> size_t {
+            size_t h1 = std::hash<float>()(vec.x);
+            size_t h2 = std::hash<float>()(vec.y);
+            return h1 ^ (h2 << 1);
+        };
+
+        size_t h = hashVec3(v.position);
+        h ^= hashVec3(v.normal) + 0x9e3779b9 + (h << 6) + (h >> 2); // boost-like hash combine
+        h ^= hashVec2(v.texCoords) + 0x9e3779b9 + (h << 6) + (h >> 2);
+        return h;
+    }
+};
+
 #endif
